3b929b33442b32c262558ded8b60ef0807772f6e
clamslam12 <35354071+clamslam12@users.noreply.github.com>
2020-02-25 15:14:50 -0700

    commit

--- server.js
+++ server.js
@@ -111,11 +111,19 @@ router.get('/movies', function(req, res) {
 router.post('/movies', function(req, res) {
     console.log(req);
     var res1 = getJSONObject(req)
-    res.send({status: res1.status, message: "movie updated", headers: res1.headers, query: res1.query, env: res1.key});
+    res.send({status: res1.status, message: "movie saved", headers: res1.headers, query: res1.query, env: res1.key});
 
 
 
 });
+
+router.route('/movies')
+    .put(authJwtController.isAuthenticated, function (req, res) {
+        var res1 = getJSONObject(req)
+        res.send({status: res1.status, message: "movie updated", headers: res1.headers, query: res1.query, env: res1.key});
+
+        }
+    );
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
